3cd05e37e88042dc34a34536f0be7b49038a65fd
apmoskevitz <andrew.moskevitz@gdbiw.com>
2023-06-28 09:38:23 -0400

    removed use of JsonConvert

--- src/Microsoft.TestPlatform.CommunicationUtilities/JsonDataSerializer.cs
+++ src/Microsoft.TestPlatform.CommunicationUtilities/JsonDataSerializer.cs
@@ -5,15 +5,15 @@ using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.IO;
-using System.Runtime.Serialization;
+using System.Text;
 
 using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.Interfaces;
 using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.Serialization;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Microsoft.VisualStudio.TestPlatform.Utilities;
 
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Newtonsoft.Json.Serialization;
 
 namespace Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
 
@@ -28,6 +28,7 @@ public class JsonDataSerializer : IDataSerializer
 
     private static readonly JsonSerializer PayloadSerializerV1; // payload serializer for version <= 1
     private static readonly JsonSerializer PayloadSerializerV2; // payload serializer for version >= 2
+    private static readonly JsonSerializer FastSerializer;
     private static readonly JsonSerializerSettings FastJsonSettings; // serializer settings for faster json
     private static readonly JsonSerializerSettings JsonSettings; // serializer settings for serializer v1, which should use to deserialize message headers
     private static readonly JsonSerializer Serializer; // generic serializer
@@ -94,6 +95,8 @@ public class JsonDataSerializer : IDataSerializer
             MaxDepth = jsonSettings.MaxDepth,
             ContractResolver = contractResolver,
         };
+
+        FastSerializer = JsonSerializer.Create(FastJsonSettings);
 
         PayloadSerializerV1.ContractResolver = new TestPlatformContractResolver1();
         PayloadSerializerV2.ContractResolver = contractResolver;
@@ -207,7 +210,7 @@ public class JsonDataSerializer : IDataSerializer
         {
             // PERF: Fast path is compatibile only with protocol versions that use serializer_2,
             // and this is faster than deserializing via deserializer_2.
-            var messageWithPayload = JsonConvert.DeserializeObject<PayloadedMessage<T>>(rawMessage, FastJsonSettings);
+            var messageWithPayload = DeserializeObjectFast<PayloadedMessage<T>>(rawMessage);
 
             return messageWithPayload == null ? default : messageWithPayload.Payload;
         }
@@ -221,6 +224,12 @@ public class JsonDataSerializer : IDataSerializer
             TPDebug.Assert(rawMessagePayload is not null, "rawMessagePayload should not be null");
             return Deserialize<T>(payloadSerializer, rawMessagePayload);
         }
+    }
+
+    private static T? DeserializeObjectFast<T>(string value)
+    {
+        using JsonTextReader reader = new(new StringReader(value));
+        return (T?)FastSerializer.Deserialize(reader, typeof(T));
     }
 
     private static bool FastHeaderParse(string rawMessage, out int version, out string? messageType)
@@ -380,7 +389,7 @@ public class JsonDataSerializer : IDataSerializer
         }
         else
         {
-            return JsonConvert.SerializeObject(new VersionedMessageForSerialization { MessageType = messageType, Version = version, Payload = payload }, FastJsonSettings);
+            return Serialize(FastSerializer, new VersionedMessageForSerialization { MessageType = messageType, Version = version, Payload = payload });
         }
     }
 
